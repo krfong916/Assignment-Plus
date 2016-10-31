efad7faddfe4c708ce21e6d7b8315d4946e8dcf4
PencilCrayons <Roliver399@gmail.com>
2016-10-31 02:01:18 -0700

    Basic Project files with storyboard implementation
    not sure how to open now since the project file to open everything is tied to a local directory, the main storyboard is the only thing with anything but it's messed up because i switched the views and need to implement autoscaling cause all the screen sizes and whatnot.

---
+++ Assignment Plus/Assignment Plus/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Assignment Plus
+//
+//  Created by rdoliver on 10/30/16.
+//  Copyright © 2016 Assignment Plus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
