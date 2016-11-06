a851c68b6405a583d61982592a94eec86e766da2
Kyle Fong <krfong@ucsc.edu>
2016-11-05 19:20:05 -0700

    Added Storyboard, adding code and PHP file next

---
+++ AssignmentPlusProto/AssignmentPlusProto/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  AssignmentPlusProto
+//
+//  Created by Kyle Fong on 10/31/16.
+//  Copyright © 2016 CMPS 115. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    
+    override func viewDidAppear(animated: Bool) {
+        self.performSegueWithIdentifier("loginView", sender: self);
+    }
+    
+
+}
+
