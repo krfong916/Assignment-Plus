f621628c44f77462f54a03b699375e3c7b1743af
Kyle Fong <krfong@ucsc.edu>
2016-11-21 03:12:28 -0800

    Added signOut and bare bones of adding a class

--- AssignmentPlusProto/AssignmentPlusProto/teacherHomeViewController.swift
+++ AssignmentPlusProto/AssignmentPlusProto/teacherHomeViewController.swift
@@ -9,23 +9,34 @@
 import UIKit
 import Firebase
 import FirebaseDatabase
+import FirebaseAuth
 
 class teacherHomeViewController: UIViewController {
     
     @IBOutlet weak var theDate: UILabel!
+    @IBAction func logout(_ sender: Any) {
+        if FIRAuth.auth()?.currentUser != nil{
+            do {
+                try FIRAuth.auth()?.signOut()
+                print("Teacher has logged out")
+                let viewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "studentOrTeacherView")
+                present(viewController, animated: true, completion: nil)
+            } catch let error as NSError {
+                print(error.localizedDescription)
+            }
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         theDate.text = DateFormatter.localizedString(from: NSDate() as Date, dateStyle:DateFormatter.Style.full, timeStyle: DateFormatter.Style.none)
 
     }
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
     func getTeacherInformation(){
         let information = FIRDatabase.database().reference()
         information.child("Teacher")
